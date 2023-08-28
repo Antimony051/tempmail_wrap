e9a7601ef35310f7b7d6c528f44615440a335d77
Antimony051 <72787497+Antimony051@users.noreply.github.com>
2023-08-28 02:32:52 +0000

    Update emailchecker.py

--- emailchecker.py
+++ emailchecker.py
@@ -1,7 +1,7 @@
 import requests
-emails=["dfserdhgseserser@mailto.plus"]
+emails=["youremailhere@mailto.plus"]        # list of emails whose inbox you want to check
 
-def download_attachment(fname,id,mail,num): # to do, make this asincronoys, so that we can download multiple attachments in parallel
+def download_attachment(fname,id,mail,num): # to do, make this asynchronous, so that we can download multiple attachments in parallel
     get_data=f"https://tempmail.plus/api/mails/{id}/attachments/{num}?email={mail}"
     r = requests.get(url = get_data, params = {})
     with open(fname,"wb") as f:
@@ -11,18 +11,18 @@ def get_email(id,mail):
     email_url= f"https://tempmail.plus/api/mails/{id}?email={mail}"
     r = requests.get(url = email_url, params = {})
 
-
-for email in emails:
-    check_inbox= "https://tempmail.plus/api/mails?email="+email # check if email has arrived.
-
-    r = requests.get(url = check_inbox, params = {})
-    response = r.json()
-    no_of_email=response["count"]
-
-    if no_of_email > 0:
-        for x in range(no_of_email):
-            mail_id= response["mail_list"][x]["mail_id"]
-            subject=response["mail_list"][x]["subject"]
-    else:
-        continue
+if __name__=="main":
+    for email in emails:
+        check_inbox= "https://tempmail.plus/api/mails?email="+email # check if email has arrived.
+    
+        r = requests.get(url = check_inbox, params = {})
+        response = r.json()
+        no_of_email=response["count"]
+    
+        if no_of_email > 0:
+            for x in range(no_of_email):
+                mail_id= response["mail_list"][x]["mail_id"]
+                subject=response["mail_list"][x]["subject"]
+        else:
+            continue
 
